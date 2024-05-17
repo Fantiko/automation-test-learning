64810183fdd8c23305261a70fee0298b4efa97d1
Fantiko <kaiostefan@gmail.com>
2024-05-17 15:15:19 -0300

    move things

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-     
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/reports/junit/test-results-[hash].xml',
+      toConsole : true
+    },
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
-    
   },
-
 });
