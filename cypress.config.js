cb1f61363cf6878b47ab07c345f01d38e34b6cf6
Fantiko <kaiostefan@gmail.com>
2024-05-17 14:06:36 -0300

    testar

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      // baseUrl:'https://cartao-de-visitas-1f916.web.app',
-    // video: true,
-    
+     
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
